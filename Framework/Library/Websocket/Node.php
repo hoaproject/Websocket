0c29d2fe854b4b15904d3e2d3be1269089e6db02
hywan <devnull@localhost>
2010-11-21 01:50:39 +0000

    Remove the require_once 'Core.php' instruction to remove all include_path dependencies.

--- Framework/Library/Websocket/Node.php
+++ Framework/Library/Websocket/Node.php
@@ -33,11 +33,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Socket_Connection_Node
  */
 import('Socket.Connection.Node');
--- Framework/Library/Websocket/Server.php
+++ Framework/Library/Websocket/Server.php
@@ -33,11 +33,6 @@
  */
 
 /**
- * Hoa_Core
- */
-require_once 'Core.php';
-
-/**
  * Hoa_Socket_Connection_Server
  */
 import('Socket.Connection.Server');
