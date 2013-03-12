62d04bd3aaacb64662ac0f7554c533e2e9eefc2d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-03-12 15:17:09 +0100

    Replace an exception by a close().

--- Server.php
+++ Server.php
@@ -377,9 +377,7 @@ class Server implements \Hoa\Core\Event\Listenable {
                       break;
 
                     default:
-                        throw new Exception(
-                            'Opcode 0x%x is not supported by this server.',
-                            0, $frame['opcode']);
+                        $this->close(self::CLOSE_DATA_ERROR);
                 }
             }
             catch ( \Hoa\Core\Exception\Idle $e ) {
