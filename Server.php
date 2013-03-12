327218a28492245a8c5e9ef2d74c9f7025b19043
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-03-12 17:04:51 +0100

    Format code. #mania

--- Server.php
+++ Server.php
@@ -375,7 +375,7 @@ class Server implements \Hoa\Core\Event\Listenable {
                     case self::OPCODE_PING:
                         $message = &$frame['message'];
 
-                        if(   0    === $frame['fin']
+                        if(   0x0  === $frame['fin']
                            || 0x7d  <  strlen($message)) {
 
                             $this->close(self::CLOSE_PROTOCOL_ERROR);
