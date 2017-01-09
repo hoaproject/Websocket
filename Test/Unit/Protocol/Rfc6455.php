96a588a0a262c0fd263166850c714b14b7b1ae30
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-01-09 14:50:50 +0100

    Quality: Happy new year!

--- Test/Unit/Protocol/Rfc6455.php
+++ Test/Unit/Protocol/Rfc6455.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -47,7 +47,7 @@ use Mock\Hoa\Websocket\Protocol\Rfc6455 as SUT;
  *
  * Test suite for the RFC6455 protocol implementation.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 class Rfc6455 extends Test\Unit\Suite
